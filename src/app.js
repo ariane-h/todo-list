54dedde87bf01466632501200e5cadb1bb7e970e
Ariane Holford <aribomop@gmail.com>
2020-03-30 17:36:45 +0100

    MVP

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Vue({
+        el: "#app",
+        data: {
+           todos: ["Buy shopping", "Clean bathroom", "Car's MOT"]
+        },
+        methods: {
+            saveNewTask: function() {
+                this.todos.push(this.newTask);
+                this.newTask = "";
+            },
+        }
+    });
+});
